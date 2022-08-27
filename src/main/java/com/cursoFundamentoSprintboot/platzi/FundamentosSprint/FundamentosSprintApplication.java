75f735f29f56dcba80efb8f575cc1b3b771b855e
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-08-27 09:52:00 -0500

    Primer Commit

---
+++ src/main/java/com/cursoFundamentoSprintboot/platzi/FundamentosSprint/FundamentosSprintApplication.java
@@ -0,0 +1,13 @@
+package com.cursoFundamentoSprintboot.platzi.FundamentosSprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoFundamentoSprintboot/platzi/FundamentosSprint/FundamentosSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoFundamentoSprintboot.platzi.FundamentosSprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
